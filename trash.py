1b16298610d664e3a82f80242cf196ef295738a2
Haikal (uif55735) <muhammad.haikal.rafiq@continental-corporation.com>
2022-10-21 13:00:07 +0800

    Fixed divide

--- trash.py
+++ trash.py
@@ -6,7 +6,7 @@ def multiply(arg1, arg2):
     return arg1 * arg2
 
 def divide(arg1, arg2):
-    return arg1 * arg2
+    return arg1 / arg2
 
 def subtract(arg1, arg2):
     return arg1 / arg2 if (arg1 > arg2) else arg2 - arg1
